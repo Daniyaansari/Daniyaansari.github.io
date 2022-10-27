3ba3c77e912912c72b039447097b01a2f9fed171
Daniyaansari <daniyaansari618@gmail.com>
2022-10-27 12:16:34 +0530

    first commit

---
+++ module.js
@@ -0,0 +1,14 @@
+//export
+// export let str = "Hello module";
+// export function print() {
+//      return "Hello this is print";
+// }
+
+let str= "Helloo";
+function print() {
+       return "This is print() function"
+};
+export { str, print};
+
+// default export (export only one thing)
+export default print;
